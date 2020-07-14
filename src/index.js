878d4c7d71ed4d8c657fc6c857a9c2905cba1775
sean som <seanartsom@gmail.com>
2020-07-15 01:58:48 +0800

    ww

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
+// to build and run live-server, use npm start command
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'tachyons';
+
+//  import UI components
+import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// render the components inside the func
+ReactDOM.render(<Hello greeting={'This is a greeting!'} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
