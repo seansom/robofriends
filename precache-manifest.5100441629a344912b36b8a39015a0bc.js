56e33b365f3b6898e4ed18bbacaebbf312568ce0
sean som <seanartsom@gmail.com>
2020-07-18 23:21:57 +0800

    Updates

---
+++ precache-manifest.5100441629a344912b36b8a39015a0bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f23e61028214c716ca49edd5c31a74",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be1b194bac4c41b4c390",
+    "url": "/robofriends/static/css/main.ef77f06c.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1b194bac4c41b4c390",
+    "url": "/robofriends/static/js/main.546f13dd.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
